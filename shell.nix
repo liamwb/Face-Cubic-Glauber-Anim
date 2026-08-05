7fbee7e1c5f6cd8401ef76af0279f3b93527fe3c
liamwb <liam.woodbaker@gmail.com>
2026-08-05 13:11:23 +1000

    pinned a version of python which works

--- shell.nix
+++ shell.nix
@@ -1,5 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }: let
-  libraryPath = with pkgs;
+{
+  pkgs ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/80c24eeb9ff46aa99617844d0c4168659e35175f.tar.gz";
+    sha256 = "sha256:0a2cws2hhdi4j5ipn1rsk5k7b6vw1l29ibb020bvgl9mza51psgl";
+  }) { },
+}:
+let
+  libraryPath =
+    with pkgs;
     lib.makeLibraryPath [
       # add other library packages here if needed
       stdenv.cc.cc
@@ -13,11 +20,10 @@ pkgs.mkShellNoCC {
     python3Packages.numpy
     python3Packages.matplotlib
     python3Packages.debugpy
-    python3Packages.snakeviz  # profiler visualisation
+    python3Packages.snakeviz # profiler visualisation
   ];
   shellHook = ''
     # export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${libraryPath}"
     fish
   '';
 }
-
