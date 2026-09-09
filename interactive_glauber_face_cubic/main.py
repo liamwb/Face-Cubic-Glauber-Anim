042b90720e9b7c57a678020591f23c3f849cdf10
liamwb <liam.woodbaker@gmail.com>
2026-09-09 03:37:34 +0000

    refactored for numba, generated critical ising pictures

--- interactive_glauber_face_cubic/main.py
+++ interactive_glauber_face_cubic/main.py
@@ -8,9 +8,13 @@ from matplotlib.patches import Rectangle
 import matplotlib.animation as animation
 import matplotlib.cm as cm
 
+from PIL import Image
+
 import time
 import argparse
 import os
+
+from numba import njit
 
 #  THE VISION:
 #
@@ -36,7 +40,10 @@ import os
 ###################
 # For the simulation on the complete graph, 64x64 seems about right
 # For the simulation on the lattice graph, 64x64 seems about right
-GRID = 212
+LATTICE_GRID=1080
+COMPLETE_GRID=128
+
+GRID = LATTICE_GRID
 TOTAL_SPINS = GRID**2
 
 CURRENT_D = 1  # single source of truth 
@@ -57,17 +64,17 @@ CURRENT_GRAPH = GraphGeometry.LATTICE
 # HELPER FUNCTIONS #
 ####################
 colors_simple = [
-    '#FF00FF',  # -5: Magenta
-    '#00FFFF',  # -4: Cyan 
-    '#FFA500',  # -3: Orange
-    '#0080FF',  # -2: Blue
-    '#FFFF00',  # -1: Yellow
+    '#9FE58E',  # -5: Soft sage
+    '#E58EC2',  # -4: Soft pink
+    '#9F8EE5',  # -3: Soft lavender
+    '#8EB1E5',  # -2: Soft periwinkle
+    '#8EE5E5',  # -1: Soft cyan
     '#FFFFFF',  #  0: Black (unused)
-    '#00FF00',  #  1: Green
-    '#FF0000',  #  2: Red
-    '#0000FF',  #  3: Dark Blue
-    '#FF8C00',  #  4: Dark Orange
-    '#800080',  #  5: Purple
+    '#E58E8E',  #  1: Soft rose
+    '#E5C28E',  #  2: Soft peach
+    '#D4E58E',  #  3: Soft lime
+    '#8EE5B1',  #  4: Soft mint
+    '#D48EE5',  #  5: Soft lilac
 ]
 
 # Create colormap
@@ -95,9 +102,11 @@ def proportions_from_state_unormalised(state):
     return np.array(prop) 
     # Note that the simulation could be made more efficient by keeping track of the proportions vector with each update, instead of counting everything each step.
 
+@njit
 def magnetisation_from_propotion(proportion):
     return np.array([proportion[i] - proportion[i+MAX_D] for i in range(MAX_D)])
 
+@njit
 def get_prop_index(spin):
     """
     Given a spin -MAX_D, ..., -1, 1, ..., MAX_D, returns the index of a proportions vector that corresponds.
@@ -108,6 +117,7 @@ def get_prop_index(spin):
     else:  # need to start from MAX_D
         return MAX_D + (- spin) - 1
 
+@njit
 def get_spin(prop_index):
     """
     Given a proportions index, return the corresponding spin
@@ -117,7 +127,7 @@ def get_spin(prop_index):
     else:  # spin is negative
         return MAX_D - prop_index - 1
 
-
+@njit
 def get_magnetisation(mag, spin):
     """
     Given a magnetisation vector and a spin, returns the magnetisation in the corresponding direction
@@ -127,6 +137,7 @@ def get_magnetisation(mag, spin):
     else:  # if the spin is negative, return whatever is in the vector *-1
         return - mag[abs(spin)-1]
 
+@njit
 def get_prop_update(spin):
     """
     Given a spin, generates a vector with a 1 in the corresponding position. Intended use is to update proportions
@@ -151,9 +162,10 @@ def get_spin_vector(spin):
 ##############
 # SIMULATION #
 ##############
-def select_vertex():
+@njit
+def select_vertex(grid):
     """Select uniformly at random a single vertex. Returns a tuple containing the coordinates of the vertex"""
-    return (np.random.choice(range(GRID)), np.random.choice(range(GRID)))
+    return (np.random.randint(0,grid), np.random.randint(0,grid))
 
 def g(i, mag, d):
     """
@@ -161,91 +173,107 @@ def g(i, mag, d):
     i should be the spin, *not* the index corresponding to that spin
     """
     numerator = exp(BETA * get_magnetisation(mag, i))
-    denominator = sum([2*cosh(BETA * get_magnetisation(mag, j)) for j in range(1, CURRENT_D+1)])
-
+    denominator = sum([2*cosh(BETA * get_magnetisation(mag, j)) for j in range(1, d+1)])
     return numerator / denominator
 
-def sample_new_spin_complete(current_spin, current_prop, d):
+@njit
+def g_num(i, mag, beta):
+    """
+    Returns the numerator of g^i (mag) := exp(beta*mag[i])/sum_j(2cosh(beta*mag[j])).
+    i should be the spin, *not* the index corresponding to that spin
+    """
+    return exp(beta * get_magnetisation(mag, i))
+
+@njit
+def g_denom(mag, beta, d):
+    """
+    Returns the denominator of g^i (mag) := exp(beta*mag[i])/sum_j(2cosh(beta*mag[j])).
+    i should be the spin, *not* the index corresponding to that spin
+    """
+    return sum([2*cosh(beta * get_magnetisation(mag, j)) for j in range(1, d+1)])
+
+@njit
+def sample_new_spin_complete(current_spin, current_prop, beta, d, total_spins):
 
     # remove the current spin from the magnetisation
     adj_prop = current_prop - get_prop_update(current_spin) 
-    adj_mag = magnetisation_from_propotion(adj_prop) / TOTAL_SPINS
+    adj_mag = magnetisation_from_propotion(adj_prop) / total_spins
 
     # compute the transition probabilities
     # only compute spins that we are currently using
-    positive_spin_probs = [g(i, adj_mag, d) if i <= CURRENT_D else 0 for i in range(1, MAX_D+1)]
-    negative_spin_probs = [g(-i, adj_mag, d) if i <= CURRENT_D else 0 for i in range(1, MAX_D+1)]
-
-    conditional_measure = positive_spin_probs + negative_spin_probs
-    
-    # choose a new spin according to our conditional measure
-    cdf = [0 for _ in conditional_measure]
-    cdf[0] = conditional_measure[0]
-    for i in range(1, len(cdf)):
-        cdf[i] = cdf[i-1] + conditional_measure[i]
-    # zero out the un-used spins (this feels inefficient)
-    for i in range(len(cdf)):
-        if abs(get_spin(i)) > CURRENT_D: cdf[i] = 0
-
-    # sample via unif(0,1) noise
-    unif = np.random.uniform(0,1)
-    # choose the largest index where the cdf is still bigger than the noise
-    below = filter( 
-        lambda i : unif <= cdf[i],
-        list(range(2*MAX_D))
-    )
-    res = min(list(below))
-
-    return get_spin(res)
-
-def sample_new_spin_lattice(vertex, state, d):
-    """
-    Sample a new spin according to the conditional face-cubic measure on the square lattice. Does periodic boundary conditions by treating opposite edges as adjacent.
-
-    vertex is a tuple containing the coordinates of the vertex to be updated
-    """
-    # compute adjacent magnetisation  (% ==> periodic)
-    left_i = ((vertex[0] - 1) % GRID, vertex[1] % GRID) 
-    right_i = ((vertex[0] + 1) % GRID, vertex[1] % GRID) 
-    top_i = (vertex[0]  % GRID, (vertex[1] + 1) % GRID) 
-    bottom_i = (vertex[0]  % GRID, (vertex[1] - 1) % GRID) 
-
-    left = get_spin_vector(state[left_i])
-    right = get_spin_vector(state[right_i])
-    top = get_spin_vector(state[top_i])
-    bottom = get_spin_vector(state[bottom_i])
-
-    adj_mag = left + right + top + bottom 
-
-    # compute transition probabilities
-    # only compute spins that we are currently using
-    positive_spin_probs = [g(i, adj_mag, d) if i <= CURRENT_D else 0 for i in range(1, MAX_D+1)]
-    negative_spin_probs = [g(-i, adj_mag, d) if i <= CURRENT_D else 0 for i in range(1, MAX_D+1)]
+    denominator = g_denom(adj_mag, beta, d)
+    positive_spin_probs = [g_num(i, adj_mag, beta)/denominator if i <= d else 0 for i in range(1, MAX_D+1)]
+    negative_spin_probs = [g_num(-i, adj_mag, beta)/denominator if i <= d else 0 for i in range(1, MAX_D+1)]
 
     conditional_measure = positive_spin_probs + negative_spin_probs
 
     # construct cdf
-    cdf = [0 for _ in conditional_measure]
-    cdf[0] = conditional_measure[0]
-    for i in range(1, len(cdf)):
-        cdf[i] = cdf[i-1] + conditional_measure[i]
-    # zero out the un-used spins (this feels inefficient)
-    for i in range(len(cdf)):
-        if abs(get_spin(i)) > CURRENT_D: cdf[i] = 0
+    cdf = np.cumsum(np.array(conditional_measure))
 
     # sample via unif(0,1) noise
     unif = np.random.uniform(0,1)
     # choose the largest index where the cdf is still bigger than the noise
-    below = filter( 
-        lambda i : unif <= cdf[i],
-        list(range(2*MAX_D))
-    )
-    res = min(list(below))
+    res=d  # If due to some fp weirdness unif > cdf[i], set to the last spin
+    for i in range(2*MAX_D):
+        if unif <= cdf[i]:
+            res=i
+            break
+
+    return get_spin(res)
+
+@njit
+def sample_new_spin_lattice(i, j, state, beta, d):
+    """
+    Sample a new spin according to the conditional face-cubic measure on the square lattice. Does periodic boundary conditions by treating opposite edges as adjacent.
+
+    i and j are the the coordinates of the vertex to be updated
+    """
+    grid=state.shape[0]
+    # compute adjacent magnetisation  (% ==> periodic boundary conditions)
+    left_i = ((i - 1) % grid, j % grid) 
+    right_i = ((i + 1) % grid, j % grid) 
+    top_i = (i  % grid, (j + 1) % grid) 
+    bottom_i = (i  % grid, (j - 1) % grid) 
+
+    left = state[left_i]
+    right = state[right_i]
+    top = state[top_i]
+    bottom = state[bottom_i]
+
+    # compute adjacent magnetisation
+    adj_mag = np.zeros(MAX_D)
+
+    for spin in [left, right, top, bottom]:
+        adj_mag[abs(spin)-1] += 1 if spin > 0 else -1
+
+    # compute transition probabilities
+    # only compute spins that we are currently using
+    denominator = g_denom(adj_mag, beta, d)
+    positive_spin_probs = [g_num(i, adj_mag, beta)/denominator if i <= d else 0 for i in range(1, MAX_D+1)]
+    negative_spin_probs = [g_num(-i, adj_mag, beta)/denominator if i <= d else 0 for i in range(1, MAX_D+1)]
+
+    conditional_measure = positive_spin_probs + negative_spin_probs
+
+    # construct cdf
+    cdf = np.cumsum(np.array(conditional_measure))
+
+    # sample via unif(0,1) noise
+    unif = np.random.uniform(0,1)
+    # choose the largest index where the cdf is still bigger than the noise
+    res=d  # If due to some fp weirdness unif > cdf[i], set to the last spin
+    for i in range(2*MAX_D):
+        if unif <= cdf[i]:
+            res=i
+            break
+
     return get_spin(res)
 
 
-def save_png_sequence(output_dir, num_frames=300, d_init=1, beta_init=1.0, geometry='lattice',
-                       updates_per_frame=500):
+def save_png_sequence(output_dir, num_frames=300, 
+                      d_init=1, 
+                      beta_init=1.0, 
+                      geometry='lattice',
+                      updates_per_frame=500):
     """Save a PNG sequence of just the grid (no UI elements) to the specified directory."""
     global CURRENT_D, BETA, CURRENT_GRAPH, GRID, TOTAL_SPINS, state
 
@@ -253,42 +281,47 @@ def save_png_sequence(output_dir, num_frames=300, d_init=1, beta_init=1.0, geome
     BETA = beta_init
     geometry_map = {'lattice': GraphGeometry.LATTICE, 'complete': GraphGeometry.COMPLETE}
     CURRENT_GRAPH = geometry_map[geometry]
-    GRID = 212 if CURRENT_GRAPH == GraphGeometry.LATTICE else 64
+    GRID = LATTICE_GRID if CURRENT_GRAPH == GraphGeometry.LATTICE else COMPLETE_GRID
     TOTAL_SPINS = GRID**2
 
     state = np.array([[unif_spin() for _ in range(GRID)] for _ in range(GRID)])
 
-    fig_save, ax_save = plt.subplots()
-    ax_save.axis('off')
-    fig_save.subplots_adjust(0, 0, 1, 1)
-
-    grid_save = ax_save.imshow(state, origin='lower', cmap=cmap_simple, norm=boundary_norm)
+    # fig_save, ax_save = plt.subplots()
+    # ax_save.axis('off')
+    # fig_save.subplots_adjust(0, 0, 1, 1)
+    #
+    # grid_save = ax_save.imshow(state, origin='lower', cmap=cmap_simple, norm=boundary_norm)
 
     os.makedirs(output_dir, exist_ok=True)
 
     for frame in range(num_frames):
         for _ in range(updates_per_frame):
-            v = select_vertex()
+            v = select_vertex(grid=GRID)
             if CURRENT_GRAPH == GraphGeometry.COMPLETE:
                 new_spin = sample_new_spin_complete(
                     current_spin=state[v],
                     current_prop=proportions_from_state_unormalised(state),
-                    d=CURRENT_D
+                    beta=BETA,
+                    d=CURRENT_D,
+                    total_spins=TOTAL_SPINS
                 )
             else:
                 new_spin = sample_new_spin_lattice(
-                    vertex=v,
+                    i=v[0],
+                    j=v[1],
                     state=state,
+                    beta=BETA,
                     d=CURRENT_D
                 )
             state[v] = new_spin
 
-        grid_save.set_data(state)
-        fig_save.savefig(os.path.join(output_dir, 'frame_{:04d}.png'.format(frame)),
-                         bbox_inches='tight', pad_inches=0)
+        rgba = cmap_simple(boundary_norm(state))          # (GRID, GRID, 4), floats 0..1
+        img = Image.fromarray((rgba * 255).astype(np.uint8), 'RGBA')
+        img.save(os.path.join(output_dir, f'frame_{frame:04d}.png'))
+
         print('Saved frame {}/{}'.format(frame + 1, num_frames), end='\r')
 
-    plt.close(fig_save)
+    # plt.close(fig_save)
     print('\nDone! {} frames saved to {}/'.format(num_frames, output_dir))
 
 
@@ -427,11 +460,11 @@ if __name__ == '__main__':
             if CURRENT_GRAPH == GraphGeometry.LATTICE:
                 button.label.set_text("COMPLETE GRAPH")
                 CURRENT_GRAPH = GraphGeometry.COMPLETE
-                GRID = 64
+                GRID = COMPLETE_GRID
             elif CURRENT_GRAPH == GraphGeometry.COMPLETE:
                 button.label.set_text("SQUARE LATTICE")
                 CURRENT_GRAPH = GraphGeometry.LATTICE
-                GRID = 212
+                GRID = LATTICE_GRID
 
             # re-compute normalising constant
             TOTAL_SPINS = GRID**2
@@ -474,18 +507,22 @@ if __name__ == '__main__':
             for _ in range(args.updates_per_frame):
                 t1 = time.perf_counter()
                 # perform a Glauber update
-                v = select_vertex()
+                v = select_vertex(grid=GRID)
 
                 if CURRENT_GRAPH == GraphGeometry.COMPLETE:
                     new_spin = sample_new_spin_complete(
                         current_spin=state[v], 
                         current_prop=proportions_from_state_unormalised(state), 
-                        d=CURRENT_D
+                        beta=BETA,
+                        d=CURRENT_D,
+                        total_spins=TOTAL_SPINS
                     ) 
                 elif CURRENT_GRAPH == GraphGeometry.LATTICE:
                     new_spin = sample_new_spin_lattice(
-                        vertex=v, 
+                        i=v[0], 
+                        j=v[1], 
                         state=state, 
+                        beta=BETA,
                         d=CURRENT_D
                     )
 
